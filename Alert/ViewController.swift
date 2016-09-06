14eebe17562da7aeb18e04dd177bcdce6d95c8be
Hakchin Kim <sn2ro@eerfmai.local>
2016-09-06 23:37:58 +0900

    aaa

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivwLamp: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnLampOn(sender: UIButton) {
+    }
 
+    @IBAction func btnLampOff(sender: UIButton) {
+    }
+    
+    @IBAction func btnLampRemove(sender: UIButton) {
+    }
+    
+    
+    
 }
 
